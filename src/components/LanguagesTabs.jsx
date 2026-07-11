75b055059876aee6b6f5c6a1da3b42c5f2f6372f
Emanuele Favero <emafaux89@gmail.com>
2026-07-11 17:35:53 +0200

    feat: add exercise logic

--- src/components/LanguagesTabs.jsx
+++ src/components/LanguagesTabs.jsx
@@ -1,5 +1,46 @@
 import { languages } from '../data/languages.js';
+import './LanguagesTabs.css';
+import { useState } from 'react';
+
+const DEFAULT_VALUE = languages[0].id || 1;
 
 export const LanguagesTabs = () => {
-  return <code>{JSON.stringify(languages)}</code>;
+  const [activeValue, setActiveValue] = useState(DEFAULT_VALUE);
+  const activeItem = languages.find((item) => item.id === activeValue);
+
+  const handleValueChange = (value) => {
+    if (value !== activeValue) setActiveValue(value);
+  };
+
+  return (
+    // Tabs
+    <div className='tabs'>
+      {/* TabsList */}
+      <div className='tabs-list' role='tablist'>
+        {languages.map(({ id, title }) => (
+          // TabsTrigger
+          <button
+            key={id}
+            className='tabs-trigger'
+            role='tab'
+            id={`tab-${id}`}
+            onClick={() => handleValueChange(id)}
+          >
+            {title}
+          </button>
+        ))}
+      </div>
+
+      {/* TabsContent */}
+      {activeItem && (
+        <div
+          className='tabs-content'
+          role='tabpanel'
+          aria-labelledby={`tab-${activeItem.id}`}
+        >
+          {activeItem.description}
+        </div>
+      )}
+    </div>
+  );
 };
